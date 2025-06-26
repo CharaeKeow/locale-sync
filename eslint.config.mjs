76f4c005f27fc8e6e1504b1dba4e8a441765c596
Charae UE <158416982+charae-ue@users.noreply.github.com>
2025-06-26 17:53:35 +0800

    chore: setup eslint & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslint.configs.recommended,
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+
+  // Import order configuration
+  {
+    rules: {
+      // Enforce type imports
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+          disallowTypeAnnotations: true,
+        },
+      ],
+
+      // Prevent importing from the same module multiple times
+      "import/no-duplicates": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
